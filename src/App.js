deba6c92a1571fd2a574980c74f8d9b03b5e8074
Henrique Alves <henrique.a.campos@ufv.br>
2025-04-10 10:14:57 -0300

    Adiciona novos components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Home from "./components/pages/Home"
-import Company from "./components/pages/Company"
-import Contact from "./components/pages/Contact"
-import NewProject from './components/pages/NewProject'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layout/Container";
 
 function App() {
   return (
@@ -13,14 +15,14 @@ function App() {
         <Link to="/company">Company</Link>
         <Link to="/newproject">New Project</Link>
       </div>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/newproject" element={<NewProject />} />
-      </Routes>
-
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
       <p>Footer</p>
     </Router>
   );
