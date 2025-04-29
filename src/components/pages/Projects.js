4356897a558c7bcf7582f169f2e4b74a87fceb88
Henrique Alves <henrique.a.campos@ufv.br>
2025-04-29 13:46:38 -0300

    Adiciona projetos na pagina projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,34 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Message from "../layouts/Message";
 import Container from "../layouts/Container";
 import LinkButton from "../layouts/LinkButton";
 import styles from "./Projects.module.css";
+import ProjectCard from "../projects/ProjectCard";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/projects', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -18,7 +37,16 @@ function Projects() {
       </div>
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
-        <p>Projetos</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );
